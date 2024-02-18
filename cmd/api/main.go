d8ac3ecd76ea3737d0a5f8cc96b988d508f040c9
Alibek Suleimenov <alibek443@gmail.com>
2024-02-19 00:02:03 +0600

    initial commit: create back-end project of movies app

---
+++ cmd/api/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+const port = 8080
+
+type Application struct {
+	Domain string
+}
+
+func main() {
+	// set app config
+	var app Application
+	app.Domain = "example.com"
+
+	// read from command line
+
+	// connect to the database
+
+	// start a web server
+	log.Println("Starting app on port", port)
+
+	err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/alibeksuleimenov/go-movies-back-end
+
+go 1.22.0
